f49da5e62944675a489e1c771b80cc5d23285c40
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2020-07-17 18:10:28 +0300

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -12,34 +12,34 @@ class Home extends Component {
     return (
       <div className='main-bg-color'>
         <div id='page-wrap' className='App'>
-          <ReactPageScroller>
-            <div className='App-header'>
-              <Navbar className='p-3'>
-                <Navbar.Brand href='/'>
-                  <Logo />
-                </Navbar.Brand>
+          {/* <ReactPageScroller> */}
+          <div className='App-header'>
+            <Navbar className='p-3'>
+              <Navbar.Brand href='/'>
+                <Logo />
+              </Navbar.Brand>
 
-                <Navbar.Collapse className='justify-content-end p-4'>
-                  <span className='pr-3'>
-                    <LanguageSelector />
-                  </span>
-                  <Menu />
-                </Navbar.Collapse>
-              </Navbar>
+              <Navbar.Collapse className='justify-content-end p-4'>
+                <span className='pr-3'>
+                  <LanguageSelector />
+                </span>
+                <Menu />
+              </Navbar.Collapse>
+            </Navbar>
 
-              <Section1 />
-            </div>
+            <Section1 />
+          </div>
 
-            <div
-              style={{
-                height: "100%",
-                width: "100%",
-                position: "relative",
-              }}
-            >
-              sssssss
-            </div>
-          </ReactPageScroller>
+          <div
+            style={{
+              height: "100%",
+              width: "100%",
+              position: "relative",
+            }}
+          >
+            sssssss
+          </div>
+          {/* </ReactPageScroller> */}
         </div>
       </div>
     );
