8bbda642817e547173e017c59b772223dde5360b
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2020-08-04 13:12:42 +0300

    Create AdminMain.jsx

---
+++ src/Components/AdminSection/AdminMain/AdminMain.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AdminMain extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='p-5 bg-info text-center'>Admin Main</h1>
+      </div>
+    );
+  }
+}
+
+export default AdminMain;
