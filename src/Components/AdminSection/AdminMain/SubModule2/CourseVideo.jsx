9c1f10ee718682b231fa5d4db1c4157eded34a16
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2020-08-05 15:48:43 +0300

    Update CourseVideo.jsx

--- src/Components/AdminSection/AdminMain/SubModule2/CourseVideo.jsx
+++ src/Components/AdminSection/AdminMain/SubModule2/CourseVideo.jsx
@@ -4,7 +4,10 @@ class CourseVideo extends Component {
   render() {
     return (
       <div>
-        <h3 className='bg-success p-5'>{this.props.adminActiveSubModule}</h3>
+        <h3 className='bg-success p-5'>
+          {this.props.adminActiveSubModule}
+          {" Component"}
+        </h3>
       </div>
     );
   }
