0c6e7a8337df213cb0029762dd62d05b74f31a8c
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2020-08-10 14:56:27 +0300

    Update ModuleHome.jsx

--- src/Components/Module/ModuleHome.jsx
+++ src/Components/Module/ModuleHome.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Navbar, Image, Row, Col, Button } from "react-bootstrap";
+import { Navbar, Image, Row, Col, Button, Container } from "react-bootstrap";
 import NewMenu from "../NewMenu/NewMenu";
 import LanguageSelector from "../Translate/LanguageSelector";
 import EsvolLogo from "../../assets/img/logo.png";
@@ -92,75 +92,79 @@ class ModuleHome extends Component {
               <LanguageSelector />
             </span>
 
-            <NewMenu MenuItem1='MenuItem-Logout' />
+            <NewMenu
+              MenuItem1='MenuItem-Logout'
+              userActiveModule={userActiveModule}
+            />
           </Navbar.Collapse>
         </Navbar>
 
-        <Row style={{ paddingTop: "130px" }} className='w-100'>
-          <Col lg={2}>
-            <div
-              className={`${userActiveModule}-panel d-none d-lg-block ml-4 `}
-            >
-              <h3
-                className={`${
-                  userActiveModule === "Module1"
-                    ? "module-text-active Module1-text-active"
-                    : "module-text"
-                }`}
-              >
-                Stage 1
-              </h3>
+        <Container>
+          <Row style={{ paddingTop: "130px" }} className='w-100'>
+            {/* Module panel */}
+            <Col lg={3}>
+              <div className={`${userActiveModule}-panel d-none d-lg-block`}>
+                <h3
+                  className={`${
+                    userActiveModule === "Module1"
+                      ? "module-text-active Module1-text-active"
+                      : "module-text"
+                  }`}
+                >
+                  Stage 1
+                </h3>
 
-              <h3
-                className={`${
-                  userActiveModule === "Module2"
-                    ? "module-text-active Module2-text-active"
-                    : "module-text"
-                }`}
-              >
-                Stage 2
-              </h3>
+                <h3
+                  className={`${
+                    userActiveModule === "Module2"
+                      ? "module-text-active Module2-text-active"
+                      : "module-text"
+                  }`}
+                >
+                  Stage 2
+                </h3>
 
-              <h3
-                className={`${
-                  userActiveModule === "Module3"
-                    ? "module-text-active"
-                    : "module-text"
-                }`}
-              >
-                Stage 3
-              </h3>
+                <h3
+                  className={`${
+                    userActiveModule === "Module3"
+                      ? "module-text-active"
+                      : "module-text"
+                  }`}
+                >
+                  Stage 3
+                </h3>
 
-              <h3
-                className={`${
-                  userActiveModule === "Module4"
-                    ? "module-text-active"
-                    : "module-text"
-                }`}
-              >
-                Stage 4
-              </h3>
+                <h3
+                  className={`${
+                    userActiveModule === "Module4"
+                      ? "module-text-active"
+                      : "module-text"
+                  }`}
+                >
+                  Stage 4
+                </h3>
 
-              <h3
-                className={`${
-                  userActiveModule === "Module5"
-                    ? "module-text-active"
-                    : "module-text"
-                } pb-5`}
-              >
-                Stage 5
-              </h3>
-            </div>
-          </Col>
+                <h3
+                  className={`${
+                    userActiveModule === "Module5"
+                      ? "module-text-active"
+                      : "module-text"
+                  } pb-5`}
+                >
+                  Stage 5
+                </h3>
+              </div>
+            </Col>
 
-          <Col lg={10}>
-            <SubModuleBtn
-              userActiveSubModule={userActiveSubModule}
-              userActiveModule={userActiveModule}
-              onClick={this.handleOnClickSubModule}
-            />
+            <Col lg={9}>
+              {/* SubModule panel */}
+              <SubModuleBtn
+                userActiveSubModule={userActiveSubModule}
+                userActiveModule={userActiveModule}
+                onClick={this.handleOnClickSubModule}
+              />
 
-            <Col>
+              {/* SubModule Section */}
               {userActiveSubModule === "sub1" ? (
                 <SubModule1
                   userActiveModule={userActiveModule}
@@ -257,8 +261,8 @@ class ModuleHome extends Component {
                 </div>
               ) : null}
             </Col>
-          </Col>
-        </Row>
+          </Row>
+        </Container>
       </div>
     );
   }
