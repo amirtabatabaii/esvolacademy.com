eccb3ef1b6ec0b958dd5c8cdf6fe28457eb907fd
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2020-08-10 11:56:18 +0300

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,37 +7,39 @@ import Section2 from "./Section2";
 import HomeCard from "./HomeCards";
 import Footer from "../Footer/Footer";
 import NavBar from "./NavBar";
+import { Container } from "react-bootstrap";
 
 class Home extends Component {
   render() {
     return (
       <div className='main-bg-color'>
         <div id='page-wrap' className='App'>
-          {/* <ReactPageScroller> */}
-          <div className='App-header'>
-            <NavBar
-              bgColor='main-bg-color'
-              MenuItem1='MenuItem-Hakkimizda'
-              MenuItem2='MenuItem-GirisYap'
-              MenuItem3='MenuItem-Iletisim'
-              MenuItem4='MenuItem-Esvol'
-            />
-          </div>
+          <Container>
+            {/* <ReactPageScroller> */}
+            <div className='App-header'>
+              <NavBar
+                bgColor='main-bg-color'
+                MenuItem1='MenuItem-Hakkimizda'
+                MenuItem2='MenuItem-GirisYap'
+                MenuItem3='MenuItem-Iletisim'
+                MenuItem4='MenuItem-Esvol'
+              />
+            </div>
 
-          <div style={{ paddingTop: "130px" }}>
-            <Section1 />
-          </div>
+            <div style={{ paddingTop: "130px" }}>
+              <Section1 />
+            </div>
 
-          <div className='pb-5'>
-            <Animated animationIn='zoomIn' animationInDuration={1500}>
-              <HomeCard />
-            </Animated>
-          </div>
+            <div className='pb-5'>
+              <Animated animationIn='zoomIn' animationInDuration={1500}>
+                <HomeCard />
+              </Animated>
+            </div>
 
-          <div className='pb-5'>
-            <Section2 />
-          </div>
-
+            <div className='pb-5'>
+              <Section2 />
+            </div>
+          </Container>
           <Footer />
 
           {/* </ReactPageScroller> */}
