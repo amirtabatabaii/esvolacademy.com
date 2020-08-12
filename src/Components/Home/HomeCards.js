001e1e71fcd2e0f904224189c9fc08a33ec00a9a
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2020-08-12 14:24:01 +0300

    Update HomeCards.js

--- src/Components/Home/HomeCards.js
+++ src/Components/Home/HomeCards.js
@@ -2,28 +2,40 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 import Esvol from "../../assets/img/home.png";
 import CardBody from "../Home/CardBody";
+import { Animated } from "react-animated-css";
 
 function HomeCards() {
   return (
-    <Row className='w-100 mt-4'>
-      <Col lg={6} md={6} sm={6} xs={12}>
-        <CardBody
-          txt='Left-card-text'
-          ImageSrc={Esvol}
-          alt='esvol'
-          width='50%'
-        />
-      </Col>
+    <Animated animationIn='zoomIn' animationInDuration={1500}>
+      <Row className='w-100 mt-4'>
+        <Col lg={4} md={6} sm={6} xs={12}>
+          <CardBody
+            txt='first-card-text'
+            ImageSrc={Esvol}
+            alt='esvol'
+            width='80%'
+          />
+        </Col>
 
-      <Col lg={6} md={6} sm={6} xs={12}>
-        <CardBody
-          txt='right-card-text'
-          ImageSrc={Esvol}
-          alt='esvol'
-          width='50%'
-        />
-      </Col>
-    </Row>
+        <Col lg={4} md={6} sm={6} xs={12}>
+          <CardBody
+            txt='second-card-text'
+            ImageSrc={Esvol}
+            alt='esvol'
+            width='80%'
+          />
+        </Col>
+
+        <Col lg={4} md={6} sm={6} xs={12} className='mx-auto'>
+          <CardBody
+            txt='third-card-text'
+            ImageSrc={Esvol}
+            alt='esvol'
+            width='80%'
+          />
+        </Col>
+      </Row>
+    </Animated>
   );
 }
 export default HomeCards;
