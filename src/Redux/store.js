01bafdee7bd1991d576bc0f50f196b7bf5836b85
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2020-10-16 19:02:41 +0300

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -62,8 +62,9 @@ const store = createStore(
   Reducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    // ,
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
