882a27b56bd002a000bb9cde9f278e924c0b552f
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2020-11-18 18:36:29 +0300

    Update ApiUrl.js

--- src/Components/Utility/ApiUrl.js
+++ src/Components/Utility/ApiUrl.js
@@ -1,5 +1,5 @@
 // export const ApiUrlMain2 = "http://10.5.51.194:8888";
 export const ApiUrlMain2 = "https://biyoteknik.com:1443/esvol";
 // export const ApiUrlMain2 = "http://31.210.91.44:8080/esvol";
-export const ApiUrlMainVideo = "http://31.210.91.44:8080";
+export const ApiUrlMainVideo = "https://biyoteknik.com:1443";
 export const ApiUrlQuestion = "/api/exam/quiz/questions";
