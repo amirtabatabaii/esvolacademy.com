3f53fc1f00e013612e731662a301c1b57051a759
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2021-02-15 17:02:34 +0300

    Update ModuleActivity.js

--- src/Components/ViewerSection/ViewerMain/DrawerSection/ModuleActivity.js
+++ src/Components/ViewerSection/ViewerMain/DrawerSection/ModuleActivity.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Drawer } from "antd";
 import { Button } from "react-bootstrap";
+import { Collapse } from "antd";
+
+const { Panel } = Collapse;
 
 class DrawerModuleActivity extends Component {
   render() {
@@ -27,7 +30,87 @@ class DrawerModuleActivity extends Component {
             </div>
           }
         >
-          <h2 className='p-5 bg-info'>Module Activity</h2>
+          <Collapse accordion defaultActiveKey='1'>
+            <Panel
+              header='Module1'
+              key='1'
+              style={{ backgroundColor: "#fac2a7" }}
+            >
+              <Collapse accordion>
+                <Panel header='Interactive Tools' key='1'>
+                  <h5>InteractiveTools Module1</h5>
+                </Panel>
+
+                <Panel header='Quiz' key='2'>
+                  <h5>Quiz Module1</h5>
+                </Panel>
+              </Collapse>
+            </Panel>
+
+            <Panel
+              header='Module2'
+              key='2'
+              style={{ backgroundColor: "#faa5a7" }}
+            >
+              <Collapse accordion>
+                <Panel header='Interactive Tools' key='1'>
+                  <h5>InteractiveTools Module2</h5>
+                </Panel>
+
+                <Panel header='Quiz' key='2'>
+                  <h5>Quiz Module2</h5>
+                </Panel>
+              </Collapse>
+            </Panel>
+
+            <Panel
+              header='Module3'
+              key='3'
+              style={{ backgroundColor: "#f6fc95" }}
+            >
+              <Collapse accordion>
+                <Panel header='Interactive Tools' key='1'>
+                  <h5>InteractiveTools Module3</h5>
+                </Panel>
+
+                <Panel header='Quiz' key='2'>
+                  <h5>Quiz Module3</h5>
+                </Panel>
+              </Collapse>
+            </Panel>
+
+            <Panel
+              header='Module4'
+              key='4'
+              style={{ backgroundColor: "#dda2fa" }}
+            >
+              <Collapse accordion>
+                <Panel header='Interactive Tools' key='1'>
+                  <h5>InteractiveTools Module4</h5>
+                </Panel>
+
+                <Panel header='Quiz' key='2'>
+                  <h5>Quiz Module4</h5>
+                </Panel>
+              </Collapse>
+            </Panel>
+
+            <Panel
+              header='Module5'
+              key='5'
+              style={{ backgroundColor: "#9cc5f7" }}
+            >
+              <Collapse accordion>
+                <Panel header='Interactive Tools' key='1'>
+                  <h5>InteractiveTools Module5</h5>
+                </Panel>
+
+                <Panel header='Quiz' key='2'>
+                  <h5>Quiz Module5</h5>
+                </Panel>
+              </Collapse>
+            </Panel>
+          </Collapse>
         </Drawer>
       </div>
     );
