279d8ea8466152fcca90e47eb34e66058c84d73c
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2021-02-15 13:46:05 +0300

    Update PreTestAns.js

--- src/Components/ViewerSection/ViewerMain/DrawerSection/PreTestAns.js
+++ src/Components/ViewerSection/ViewerMain/DrawerSection/PreTestAns.js
@@ -25,8 +25,21 @@ class DrawerPreTestAns extends Component {
             </div>
           }
         >
-          <h2 className='p-5 bg-info'>Pre Test</h2>
-        </Drawer>
+            <h5 className='p-2 m-auto'>Age</h5>
+            <h5 className='p-2 m-auto'>Gender</h5>
+            <h5 className='p-2 m-auto'>Occupation</h5>
+            <h5 className='p-2 m-auto'>Professional Experience (years)</h5>
+            <h5 className='p-2 m-auto'>Country</h5>
+            <h5 className='p-2 m-auto'>Have you completed an online training on volunteering before?</h5>
+            <h5 className='p-2 m-auto'>Have you ever had a gamification-based online training?</h5>
+            <h5 className='p-2 m-auto'>Have you completed an online training before and received a certificate?</h5>
+            <h5 className='p-2 m-auto'>Do you have any knowledge about social leadership and volunteering in sport?</h5>
+            <h5 className='p-2 m-auto'>Do you have any knowledge about social innovation in/through sport?</h5>
+            <h5 className='p-2 m-auto'>Do you have any knowledge about social inclusion in sport?</h5>
+            <h5 className='p-2 m-auto'>Do you have any knowledge on social equality in sport?</h5>
+            <h5 className='p-2 m-auto'>Do you have any knowledge on good governance, safety and security in sport events?</h5>
+            <h5 className='p-2 m-auto'>What do you think about your level of knowledge and skills in social innovation and leadership in sports?</h5>
+         </Drawer>
       </div>
     );
   }
