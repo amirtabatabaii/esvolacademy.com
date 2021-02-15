755fb3760ca6889a417954a8ad7b470e1d31f5c1
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2021-02-15 09:26:26 +0300

    Create UserInfo.js

---
+++ src/Components/ViewerSection/ViewerMain/DrawerSection/UserInfo.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Drawer } from "antd";
+import { Button } from "react-bootstrap";
+
+class DrawerUserInfo extends Component {
+  render() {
+    const { onClose, visible } = this.props;
+
+    return (
+      <div>
+        <Drawer
+          title={<h4>User Information</h4>}
+          width={1300}
+          onClose={onClose}
+          visible={visible}
+          bodyStyle={{ paddingBottom: 80 }}
+          placement='right'
+          footer={
+            <div
+              style={{
+                textAlign: "center",
+              }}
+            >
+              <Button onClick={onClose}>{"Close User-Info Section"}</Button>
+            </div>
+          }
+        >
+          <h2 className='p-5 bg-info'>User Info </h2>
+        </Drawer>
+      </div>
+    );
+  }
+}
+export default DrawerUserInfo;
