124a7a62cb2e75b1d8823d5c3bcc17d9150e1654
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2021-02-17 14:09:58 +0300

    Update Settings.jsx

--- src/Components/UserHome/Settings.jsx
+++ src/Components/UserHome/Settings.jsx
@@ -39,7 +39,7 @@ class Settings extends Component {
             >
               <TranslateText txt='User-SaveEdit' />
             </button>
-            ,
+
             <button
               key='submit'
               className='save-Btn m-3'
