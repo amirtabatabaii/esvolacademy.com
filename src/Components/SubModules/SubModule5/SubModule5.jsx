bdb7f32fc8ce5985a484b1e8a305c52207bb80e3
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2021-04-24 17:58:23 +0300

    Update SubModule5.jsx

--- src/Components/SubModules/SubModule5/SubModule5.jsx
+++ src/Components/SubModules/SubModule5/SubModule5.jsx
@@ -9,6 +9,7 @@ class SubModule5 extends Component {
   render() {
     const {
       userActiveModule,
+      userActiveSubModule,
       SubModule5CaseStudyFiltered,
       isEasyModeActive,
       onClick,
@@ -26,16 +27,23 @@ class SubModule5 extends Component {
             />
           ))}
 
-          <SubModuleNextButton
-            // disabled={played.toFixed(2) * 100 <= SubModuleRatio ? true : false}
-            className={`${userActiveModule}-next-btn m-3`}
-            condition={!isEasyModeActive}
-            isEasyModeActive={!isEasyModeActive}
-            NormalNextSubModule='sub6'
-            SubModule6Name='sub6'
-            txt='SubModule5-Continue'
-            onClick={onClick}
-          />
+          {localStorage.getItem("UserModule").substring(6) <
+            localStorage.getItem("UserTempModule").substring(6) ||
+            (localStorage.getItem("UserModule").substring(6) ===
+              localStorage.getItem("UserTempModule").substring(6) &&
+              userActiveSubModule.substring(3) ===
+                localStorage.getItem("UserSubModule").substring(3) && (
+                <SubModuleNextButton
+                  // disabled={played.toFixed(2) * 100 <= SubModuleRatio ? true : false}
+                  className={`${userActiveModule}-next-btn m-3`}
+                  condition={!isEasyModeActive}
+                  isEasyModeActive={!isEasyModeActive}
+                  NormalNextSubModule='sub6'
+                  SubModule6Name='sub7'
+                  txt='SubModule5-Continue'
+                  onClick={onClick}
+                />
+              ))}
         </Scroll>
       </div>
     );
