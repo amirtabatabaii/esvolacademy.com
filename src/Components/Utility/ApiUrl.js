05cb2deeb8092ca3358ab4b37df6bbfde4658cd6
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2022-11-22 19:44:01 +0300

    change api url

--- src/Components/Utility/ApiUrl.js
+++ src/Components/Utility/ApiUrl.js
@@ -1,5 +1,12 @@
+// // export const ApiUrlMain2 = "http://10.5.51.194:8888";
+// export const ApiUrlMain2 = "https://biyoteknik.com:1443/esvol";
+// // export const ApiUrlMain2 = "http://31.210.91.44:8080/esvol";
+// export const ApiUrlMainVideo = "https://biyoteknik.com:1443";
+// export const ApiUrlQuestion = "/api/exam/quiz/questions";
+
+
 // export const ApiUrlMain2 = "http://10.5.51.194:8888";
-export const ApiUrlMain2 = "https://biyoteknik.com:1443/esvol";
+export const ApiUrlMain2 = "http://78.142.211.82:1443";
 // export const ApiUrlMain2 = "http://31.210.91.44:8080/esvol";
-export const ApiUrlMainVideo = "https://biyoteknik.com:1443";
+export const ApiUrlMainVideo = "http://78.142.211.82:1443";
 export const ApiUrlQuestion = "/api/exam/quiz/questions";
